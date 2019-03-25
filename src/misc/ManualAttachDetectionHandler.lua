8b46bffb4c17d84f546084f3486518bd40f69f4b
Stijn Wopereis <stijnwop@gmail.com>
2019-03-25 23:05:08 +0100

    Improved detection handler.

--- src/misc/ManualAttachDetectionHandler.lua
+++ src/misc/ManualAttachDetectionHandler.lua
@@ -1,3 +1,10 @@
+---
+-- ManualAttachDetectionHandler
+--
+-- Main class for handling the vehicle detection.
+--
+-- Copyright (c) Wopster, 2019
+
 ManualAttachDetectionHandler = {}
 
 local ManualAttachDetectionHandler_mt = Class(ManualAttachDetectionHandler)
@@ -9,6 +16,8 @@ function ManualAttachDetectionHandler:new(isServer, isClient, mission, modDirect
     self.isClient = isClient
     self.mission = mission
     self.modDirectory = modDirectory
+
+    self.triggerCloneNode = nil
     self.detectedVehicleInTrigger = {}
     self.listeners = {}
 
@@ -18,76 +27,89 @@ function ManualAttachDetectionHandler:new(isServer, isClient, mission, modDirect
     return self
 end
 
-function ManualAttachDetectionHandler.inj_onEnter(player, isControlling)
-    if isControlling then
-        g_manualAttach.detectionHandler:loadTrigger()
-    end
-end
-
-function ManualAttachDetectionHandler.inj_onLeave(player)
-    g_manualAttach.detectionHandler:unloadTrigger()
-end
-
+---Called on load.
 function ManualAttachDetectionHandler:load()
+    self:loadCloneableTrigger()
 end
 
+---Called on delete.
 function ManualAttachDetectionHandler:delete()
+    delete(self.triggerCloneNode)
 end
 
+---Adds listener from the list.
+---@param listener table
 function ManualAttachDetectionHandler:addDetectionListener(listener)
     if listener ~= nil then
         ListUtil.addElementToList(self.listeners, listener)
     end
 end
 
+---Removes listener from the list.
+---@param listener table
 function ManualAttachDetectionHandler:removeDetectionListener(listener)
     if listener ~= nil then
         ListUtil.removeElementFromList(self.listeners, listener)
     end
 end
 
+---Notifies listeners that the vehicle list has changed.
+---@param vehicles table
 function ManualAttachDetectionHandler:notifyVehicleListChanged(vehicles)
     for _, listener in ipairs(self.listeners) do
         listener:onVehicleListChanged(vehicles)
     end
 end
 
+---Notifies listeners that the trigger has been added or removed.
+---@param isRemoved boolean
 function ManualAttachDetectionHandler:notifyVehicleTriggerChange(isRemoved)
     for _, listener in ipairs(self.listeners) do
         listener:onTriggerChanged(isRemoved)
     end
 end
 
-function ManualAttachDetectionHandler:loadTrigger()
-    if self.isClient then
-        local detectionTriggerFilename = Utils.getFilename("resources/detectionTrigger.i3d", self.modDirectory)
-        local rootNode = loadI3DFile(detectionTriggerFilename, false, false, false)
-        local detectionTrigger = I3DUtil.indexToObject(rootNode, "0")
+---Loads the trigger from the i3d file.
+function ManualAttachDetectionHandler:loadCloneableTrigger()
+    local filename = Utils.getFilename("resources/detectionTrigger.i3d", self.modDirectory)
+    local rootNode = loadI3DFile(filename, false, false, false)
+    local trigger = I3DUtil.indexToObject(rootNode, "0")
 
-        unlink(detectionTrigger)
-        delete(rootNode)
+    unlink(trigger)
+    delete(rootNode)
 
-        self.detectionTrigger = detectionTrigger
-        addToPhysics(self.detectionTrigger)
-        link(getRootNode(), self.detectionTrigger)
+    self.triggerCloneNode = trigger
+    link(getRootNode(), self.triggerCloneNode)
+
+    -- Add trigger on initial load.
+    self:addTrigger()
+end
+
+---Adds the trigger to the player.
+function ManualAttachDetectionHandler:addTrigger()
+    if self.isClient and self.triggerCloneNode ~= nil then
+        self.trigger = clone(self.triggerCloneNode, false, false, false)
+
+        addToPhysics(self.trigger)
+        link(getRootNode(), self.trigger)
 
         -- Link trigger to player
-        link(self.mission.player.rootNode, self.detectionTrigger)
-        --setTranslation(self.detectionTrigger, 0, 0, 0)
+        link(self.mission.player.rootNode, self.trigger)
 
-        addTrigger(self.detectionTrigger, "vehicleDetectionCallback", self)
+        addTrigger(self.trigger, "vehicleDetectionCallback", self)
 
         self:notifyVehicleTriggerChange(false)
     end
 end
 
-function ManualAttachDetectionHandler:unloadTrigger()
+---Removes the trigger from the player.
+function ManualAttachDetectionHandler:removeTrigger()
     if self.isClient then
-        if self.detectionTrigger ~= nil then
-            removeFromPhysics(self.detectionTrigger)
-            removeTrigger(self.detectionTrigger)
-            delete(self.detectionTrigger)
-            self.detectionTrigger = nil
+        if self.trigger ~= nil then
+            removeFromPhysics(self.trigger)
+            removeTrigger(self.trigger)
+            delete(self.trigger)
+            self.trigger = nil
         end
 
         self.detectedVehicleInTrigger = {}
@@ -95,17 +117,24 @@ function ManualAttachDetectionHandler:unloadTrigger()
     end
 end
 
-function ManualAttachDetectionHandler:update(dt)
-end
-
+---Checks if the detected vehicle is valid.
+---@param vehicle table
+---returns true if valid, false otherwise.
 function ManualAttachDetectionHandler.getIsValidVehicle(vehicle)
     return vehicle ~= nil
             and vehicle.isa ~= nil
             and vehicle:isa(Vehicle)
             and not vehicle:isa(StationCrane) -- Dismiss trains and the station cranes
             and vehicle.getAttacherJoints ~= nil
+            and vehicle:getAttacherJoints() ~= nil
 end
 
+---Callback when trigger changes state.
+---@param triggerId number
+---@param otherId number
+---@param onEnter boolean
+---@param onLeave boolean
+---@param onStay boolean
 function ManualAttachDetectionHandler:vehicleDetectionCallback(triggerId, otherId, onEnter, onLeave, onStay)
     if (onEnter or onLeave) then
         local lastAmount = #self.detectedVehicleInTrigger
@@ -120,12 +149,25 @@ function ManualAttachDetectionHandler:vehicleDetectionCallback(triggerId, otherI
             else
                 ListUtil.removeElementFromList(self.detectedVehicleInTrigger, nodeVehicle)
             end
-
-            Logger.info("Amount in trigger", #self.detectedVehicleInTrigger)
         end
 
         if lastAmount ~= #self.detectedVehicleInTrigger then
             self:notifyVehicleListChanged(self.detectedVehicleInTrigger)
         end
     end
+end
+
+---Injects in the player onEnter function to load the trigger when controlling the player.
+---@param player table
+---@param isControlling boolean
+function ManualAttachDetectionHandler.inj_onEnter(player, isControlling)
+    if isControlling then
+        g_manualAttach.detectionHandler:addTrigger()
+    end
+end
+
+---Injects in the player onLeave function
+---@param player table
+function ManualAttachDetectionHandler.inj_onLeave(player)
+    g_manualAttach.detectionHandler:removeTrigger()
 end
