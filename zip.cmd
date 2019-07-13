eda5da21ca2fce7f9fe140fe28650b28de4d7bd9
Stijn Wopereis <stijnwop@gmail.com>
2019-07-13 23:36:19 +0200

    Add zip cmd file for Windows users.

---
+++ zip.cmd
@@ -0,0 +1,24 @@
+SET filename="FS19_ManualAttach.zip"
+
+IF EXIST %filename% (
+    DEL  %filename% > NUL
+)
+
+"7z.exe" a -tzip %filename% ^
+   -i!*.lua ^
+   -i!*.dds ^
+   -i!*.i3d ^
+   -i!*.i3d.shapes ^
+   -i!*.grle ^
+   -i!*.i3d.anim ^
+   -i!*.ogg ^
+   -i!*.xml ^
+   -xr!$data ^
+   -xr!Substance ^
+   -xr!.mayaSwatches ^
+   -xr!.idea ^
+   -aoa -r ^
+
+IF %ERRORLEVEL% NEQ 0 ( exit 1 )
+
+PAUSE
