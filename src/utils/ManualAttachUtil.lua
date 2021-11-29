5604282f3acb97228c7cd37f47781385728b5b10
Stijn Wopereis <stijn.wopereis@creative-mesh.com>
2021-11-29 21:54:48 +0100

    Remove FS19 edge case logic from the manual attach utility.

--- src/utils/ManualAttachUtil.lua
+++ src/utils/ManualAttachUtil.lua
@@ -1,9 +1,12 @@
 --
 -- ManualAttachUtil
 --
--- Utility for Manual Attach
+-- Author: Wopster
+-- Description: Utility for Manual Attach
+-- Name: ManualAttachUtil
+-- Hide: yes
 --
--- Copyright (c) Wopster, 2019
+-- Copyright (c) Wopster, 2021
 
 ---@class ManualAttachUtil
 ManualAttachUtil = {}
@@ -149,30 +152,6 @@ function ManualAttachUtil.hasConnectionHoses(object, vehicle)
     local hoses = object:getConnectionHosesByInputAttacherJoint(inputJointDescIndex)
 
     return #hoses ~= 0 and ManualAttachUtil.doesVehicleSupportAllHoseTypes(vehicle, attacherJointIndex, hoses)
-end
-
----Todo: Check when this dirty hack can be deleted in the next patch.
----Checks weather or not we are dealing with an edge case scenario for the connection hoses.
----Returns true when we are dealing with an edge case scenario, false otherwise.
----@param object table
----@return boolean
-function ManualAttachUtil.hasEdgeCaseHose(object)
-    if object.spec_attachable == nil then
-        return false
-    end
-
-    local inputJointDescIndex = object.spec_attachable.inputAttacherJointDescIndex
-    local hoses = object:getConnectionHosesByInputAttacherJoint(inputJointDescIndex)
-
-    local hose = hoses[1]
-    -- Total edge case with the Amazone Catros.. needs a proper fix from Giants.
-    if hose ~= nil and #hoses == 1 then
-        if hose.type == "TOOL_CONNECTOR_TOP_RIGHT" then
-            return true
-        end
-    end
-
-    return false
 end
 
 ---Returns true when object has attached connection hoses, false otherwise.
